0fc818268c9507694512903cbca0080517a20207
Peter Bemetz <peter.bemetz@t-online.de>
2020-10-29 21:53:04 +0100

    versy blatt 2

--- VERSY/src/aqua/blatt1/broker/Broker.java
+++ VERSY/src/aqua/blatt1/broker/Broker.java
@@ -1,6 +1,12 @@
 package aqua.blatt1.broker;
 
 import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import javax.swing.JOptionPane;
 
 import aqua.blatt1.common.Direction;
 import aqua.blatt1.common.Properties;
@@ -8,54 +14,81 @@ import aqua.blatt1.common.msgtypes.DeregisterRequest;
 import aqua.blatt1.common.msgtypes.HandoffRequest;
 import aqua.blatt1.common.msgtypes.RegisterRequest;
 import aqua.blatt1.common.msgtypes.RegisterResponse;
+import aqua.blatt2.broker.PoisonPill;
 import messaging.Endpoint;
 import messaging.Message;
 
 public class Broker {
 
 	private static final String TANK = "tank";
-	private int idIndex = 1;
-
+	private static final int POOL_SIZE = 10;
+	private volatile int idIndex = 1;
+	private volatile boolean stopRequested = false;
+	private ReadWriteLock lock = new ReentrantReadWriteLock();
 	private Endpoint endpoint = new Endpoint(Properties.PORT);
-	private ClientCollection<InetSocketAddress> clients = new ClientCollection<>();
+	private volatile ClientCollection<InetSocketAddress> clients = new ClientCollection<>();
 
 	public void broker() {
-		while (true) {
+		ExecutorService threadPool = Executors.newFixedThreadPool(POOL_SIZE);
+		new Thread(stopServerInterface()).start();
+		while (!stopRequested) {
 			Message actualMessage = endpoint.blockingReceive();
-			if (actualMessage.getPayload() instanceof RegisterRequest) {
-				register(actualMessage.getSender());
-			} else if (actualMessage.getPayload() instanceof DeregisterRequest) {
-				deregister(actualMessage.getSender());
-			} else if (actualMessage.getPayload() instanceof HandoffRequest) {
-				handoffFish(actualMessage);
-			} else {
-				System.out.println("The payload is an instance of an unknown object!");
-			}
+			threadPool.execute(() -> {
+				if (actualMessage.getPayload() instanceof RegisterRequest) {
+					register(actualMessage.getSender());
+				} else if (actualMessage.getPayload() instanceof DeregisterRequest) {
+					deregister(actualMessage.getSender());
+				} else if (actualMessage.getPayload() instanceof HandoffRequest) {
+					handoffFish(actualMessage);
+				} else if (actualMessage.getPayload() instanceof PoisonPill) {
+					threadPool.shutdown();
+					System.exit(0);
+				} else {
+					System.err.println("Message contains unknown payload!");
+				}
+			});
 		}
+		threadPool.shutdown();
 	}
 
-	public void register(InetSocketAddress client) {
+	private void register(InetSocketAddress client) {
+		lock.writeLock().lock();
 		String id = TANK + idIndex++;
 		clients.add(id, client);
+		lock.writeLock().unlock();
 		endpoint.send(client, new RegisterResponse(id));
 	}
 
-	public void deregister(InetSocketAddress client) {
+	private void deregister(InetSocketAddress client) {
+		lock.writeLock().lock();
 		clients.remove(clients.indexOf(client));
+		lock.writeLock().unlock();
 	}
 
-	public void handoffFish(Message message) {
+	private void handoffFish(Message message) {
 		Direction direction = ((HandoffRequest) message.getPayload()).getFish().getDirection();
 		InetSocketAddress adress;
 		if (direction == Direction.LEFT) {
+			lock.readLock().lock();
 			adress = clients.getLeftNeighorOf(clients.indexOf(message.getSender()));
+			lock.readLock().unlock();
 		} else {
+			lock.readLock().lock();
 			adress = clients.getRightNeighorOf(clients.indexOf(message.getSender()));
+			lock.readLock().unlock();
 		}
 		endpoint.send(adress, message.getPayload());
 	}
 
+	private Runnable stopServerInterface() {
+		return () -> {
+			JOptionPane.showMessageDialog(null, "Press OK button to stop server");
+			stopRequested = true;
+		};
+	}
+
 	public static void main(String[] args) {
+		System.out.println("Starting Broker...");
 		Broker broker = new Broker();
 		broker.broker();
 	}
